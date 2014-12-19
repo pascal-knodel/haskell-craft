ddf95a124309d38f76cc7661615d11dce94b2dc1
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 02:52:35 +0100

    5.1 - 5.34

---
+++ Chapter 5/C'5.hs
@@ -0,0 +1,54 @@
+--
+-- Chapter 5.
+--
+module C'5 where
+
+
+
+import Prelude hiding
+ ( 
+   toUpper
+ , isLower
+ , elem
+ )
+
+import Test.QuickCheck
+import Test.HUnit
+
+
+
+
+import E'5'34
+import E'5'33
+import E'5'32
+import E'5'31
+import E'5'30
+import E'5'29
+import E'5'28
+import E'5'27
+import E'5'26
+import E'5'25
+import E'5'24
+import E'5'23
+import E'5'22
+import E'5'21
+import E'5'20
+import E'5'19
+import E'5'18
+import E'5'17
+import E'5'16
+import E'5'15
+import E'5'14
+import E'5'13
+import E'5'12
+import E'5'11
+import E'5'10
+import E'5''9
+import E'5''8
+import E'5''7
+import E'5''6
+import E'5''5
+import E'5''4
+import E'5''3
+import E'5''2
+import E'5''1
