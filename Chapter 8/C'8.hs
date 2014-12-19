dec7e8d139db12fb010f77ddeecfa968b0436360
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 03:21:31 +0100

    8.1 - 8.23

---
+++ Chapter 8/C'8.hs
@@ -0,0 +1,43 @@
+--
+-- Chapter 8.
+--
+module C'8 where
+
+
+
+import Test.QuickCheck hiding ( Result )
+
+
+-- Notes: Chapter 4> cabal install
+--        Chapter 7> cabal install
+
+import C'4
+import C'7
+
+
+import B'C'8
+
+
+import E'8'23
+import E'8'22
+import E'8'21
+import E'8'20
+import E'8'19
+import E'8'18
+import E'8'17
+import E'8'16
+import E'8'15
+import E'8'14
+import E'8'13
+import E'8'12
+import E'8'11
+import E'8'10
+import E'8''9
+import E'8''8
+import E'8''7
+import E'8''6
+import E'8''5
+import E'8''4
+import E'8''3
+import E'8''2
+import E'8''1
