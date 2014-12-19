6953c77e673e3246185c7700c7825ddf98499a44
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 04:33:03 +0100

    3.1

---
+++ Chapter 3/E'3''1.hs
@@ -0,0 +1,28 @@
+--
+--
+--
+----------------
+-- Exercise 3.1.
+---------------
+--
+--
+--
+module E'3''1 where
+
+
+
+
+xor2 :: Bool -> Bool -> Bool
+xor2 a b 
+
+ =      a == True 
+    &&  b == False
+      
+    ||
+   
+        a == False
+    &&  b == True 
+
+
+
+
