54af81341a3d8cd27b39ee602a40a68770019d09
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 02:54:36 +0100

    11.24

---
+++ Chapter 11/E'11'24.hs
@@ -0,0 +1,22 @@
+--
+--
+--
+------------------
+-- Exercise 11.24.
+------------------
+--
+--
+--
+module E'11'24 where
+
+
+
+
+integrate :: (Float -> Float) -> (Float -> Float -> Float)
+integrate
+
+ =  undefined   -- TODO: numerical integration.
+
+
+
+
