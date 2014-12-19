53eda0c38550ba1450ba11ca6ad6fb2619915fb3
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 04:35:07 +0100

    3.27, Example

---
+++ Chapter 3/E'3'27'.hs
@@ -0,0 +1,20 @@
+--
+--
+--
+-----------------
+-- Exercise 3.27.
+-----------------
+--
+--
+--
+module E'3'27' where
+
+
+
+
+ definition argument = argument
+  other_Definition other_Argument = other_Argument
+
+
+
+
