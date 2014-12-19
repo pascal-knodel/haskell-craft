3d220e6dca633fd9c9d8854137c1790d2d6b2abc
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 02:52:31 +0100

    5.6

---
+++ Chapter 5/E'5''6.hs
@@ -0,0 +1,22 @@
+--
+--
+--
+----------------
+-- Exercise 5.6.
+----------------
+--
+--
+--
+module E'5''6 where
+
+
+
+-- type ShopItem = (String, Int)
+
+
+data ShopItem =  ShopItem String Int
+                 deriving ( Eq , Show )
+
+
+
+
