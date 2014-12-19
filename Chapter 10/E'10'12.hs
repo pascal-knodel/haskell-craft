56bea24f30632ebf3d76fe2def5f43dd258e971b
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 02:54:11 +0100

    10.12

---
+++ Chapter 10/E'10'12.hs
@@ -0,0 +1,21 @@
+--
+--
+--
+------------------
+-- Exercise 10.12.
+------------------
+--
+--
+--
+module E'10'12 where
+
+
+
+
+--   map f ( map g xs ) = map g ( map f xs )
+--
+-- Proof, see ex. 10.4.
+
+
+
+
