d6fa2d86853df6b012d4f2fa18325b2f30360b29
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 02:54:39 +0100

    11.1 - 11.36

---
+++ Chapter 11/C'11.hs
@@ -0,0 +1,58 @@
+--
+-- Chapter 11.
+--
+module C'11 where
+
+
+
+import Prelude hiding
+ (
+   id
+ , flip
+ )
+
+
+
+import B'C'11
+
+
+-- import C'6
+
+
+
+import E'11'36
+import E'11'35
+import E'11'34
+import E'11'33
+import E'11'32
+import E'11'31
+import E'11'30
+import E'11'29
+import E'11'28
+import E'11'27
+import E'11'26
+import E'11'25
+import E'11'24
+import E'11'23
+import E'11'22
+import E'11'21
+import E'11'20
+import E'11'19
+import E'11'18
+import E'11'17
+import E'11'16
+import E'11'15
+import E'11'14
+import E'11'13
+import E'11'12
+import E'11'11
+import E'11'10
+import E'11''9
+import E'11''8
+import E'11''7
+import E'11''6
+import E'11''5
+import E'11''4
+import E'11''3
+import E'11''2
+import E'11''1
