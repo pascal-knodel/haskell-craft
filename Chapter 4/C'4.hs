68d88bcd9b7cd4663487f8091407224b41ec2be2
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 03:17:15 +0100

    4.1 - 4.39

---
+++ Chapter 4/C'4.hs
@@ -0,0 +1,53 @@
+--
+-- Chapter 4.
+--
+module C'4 where
+
+
+
+import PicturesSVG
+
+import Test.QuickCheck hiding ( Result )
+import Test.HUnit
+
+
+
+import E'4'39
+import E'4'38
+import E'4'37
+import E'4'36
+import E'4'35
+import E'4'34
+import E'4'33
+import E'4'32
+import E'4'31
+import E'4'30
+import E'4'29
+import E'4'28
+import E'4'27
+import E'4'26
+import E'4'25
+import E'4'24
+import E'4'23
+import E'4'22
+import E'4'21
+import E'4'20
+import E'4'19
+import E'4'18
+import E'4'17
+import E'4'16
+import E'4'15
+import E'4'14
+import E'4'13
+import E'4'12
+import E'4'11
+import E'4'10
+import E'4''9
+import E'4''8
+import E'4''7
+import E'4''6
+import E'4''5
+import E'4''4
+import E'4''3
+import E'4''2
+import E'4''1
