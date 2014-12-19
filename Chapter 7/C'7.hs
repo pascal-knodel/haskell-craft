6ba07f671ebd062aa2767869475a91835b45111a
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 02:53:20 +0100

    7.1 - 7.35

---
+++ Chapter 7/C'7.hs
@@ -0,0 +1,67 @@
+--
+-- Chapter 7.
+--
+module C'7 where
+
+
+
+import Prelude hiding
+ ( 
+   product
+ , reverse
+ , unzip
+ , minimum
+ , maximum
+ , and
+ , or
+ , getLine
+ , splitAt
+ , take
+ , drop
+ , zip3
+ , words
+ )
+
+import Test.QuickCheck
+import QCC ( quickCheckN )
+
+import Test.HUnit
+
+
+
+
+import E'7'35
+import E'7'34
+import E'7'33
+import E'7'32
+import E'7'31
+import E'7'30
+import E'7'29
+import E'7'28
+import E'7'27
+import E'7'26
+import E'7'25
+import E'7'24
+import E'7'23
+import E'7'22
+import E'7'21
+import E'7'20
+import E'7'19
+import E'7'18
+import E'7'17
+import E'7'16
+import E'7'15
+import E'7'14
+import E'7'13
+import E'7'12
+import E'7'11
+import E'7'10
+import E'7''9
+import E'7''8
+import E'7''7
+import E'7''6
+import E'7''5
+import E'7''4
+import E'7''3
+import E'7''2
+import E'7''1
