8c36e359543744c0b2450fd9fc1a6c641b0b61f5
Pascal Knodel <pascal.knodel@mail.com>
2014-12-19 04:33:11 +0100

    3.1 - 3.27

---
+++ Chapter 3/C'3.hs
@@ -0,0 +1,46 @@
+--
+-- Chapter 3.
+--
+module C'3 where
+
+
+
+import Prelude hiding
+ (
+   (&&)
+ , (||)
+ )
+
+
+import Test.QuickCheck
+
+
+
+
+import E'3'27
+import E'3'26
+import E'3'25
+import E'3'24
+import E'3'23
+import E'3'22
+import E'3'21
+import E'3'20
+import E'3'19
+import E'3'18
+import E'3'17
+import E'3'16
+import E'3'15
+import E'3'14
+import E'3'13
+import E'3'12
+import E'3'11
+import E'3'10
+import E'3''9
+import E'3''8
+import E'3''7
+import E'3''6
+import E'3''5
+import E'3''4
+import E'3''3
+import E'3''2
+import E'3''1
