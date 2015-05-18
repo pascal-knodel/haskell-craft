15a03a50f06942b1abcfcd8c5b792fbb423a7224
Pascal Knodel <pascal.knodel@mail.com>
2015-05-18 12:44:53 +0200

    13.18

---
+++ Chapter 13/E'13'18.hs
@@ -0,0 +1,36 @@
+--
+--
+--
+------------------
+-- Exercise 13.18.
+------------------
+--
+--
+--
+module E'13'18 where
+
+
+
+-- Question: How do you show that two type descriptions don't unify?
+
+
+
+
+-- "(Int -> b)" and "(Int, a, a)":
+----------------------------------
+
+
+-- First guess: They don't unify.
+
+
+
+
+-- "(a -> Bool)" and "(a, a, [Bool])":
+--------------------------------------
+
+
+-- First guess: They don't unify.
+
+-- 346
+
+
